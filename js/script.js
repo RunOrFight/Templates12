a8df243aa152cd5995949246f8c109648b1df104
RumOrFight <dragoinfury@gmail.com>
2021-09-07 20:25:00 +0300

    add logic

--- js/script.js
+++ js/script.js
@@ -1,1 +1,40 @@
-console.log('Hello World')
+const checkbox = document.querySelector('.readonly-switch')
+checkbox.addEventListener('change', function(){
+    const addTagBtn = document.querySelector('.add-tag_button')
+    const input = document.querySelector('.add-tag_input')
+    const tagBtns = document.querySelectorAll('.tag-button')
+    addTagBtn.disabled = !addTagBtn.disabled
+    input.disabled = !input.disabled 
+    // tagBtns.map((elem)=>{
+    //     elem.disabled = !elem.disabled
+    // })
+    for (tagBtn of tagBtns){
+        tagBtn.disabled = !tagBtn.disabled
+    }
+})
+
+function addTag(){
+    const tagList = document.querySelector('.tags-area')
+    const input = document.querySelector('.add-tag_input')
+    let currentId = '0'
+    if (tagList.lastElementChild){
+        currentId = (parseInt(tagList.lastElementChild.id) + 1)
+    }
+    if(input.value.trim() != ''){
+        tagList.innerHTML += `<div class="tag" id=${currentId}><p class="tag-text">${input.value}</p><button class="tag-button" onclick="removeTag(${currentId})">+</button></div>`
+    }
+    input.value = ''
+}
+
+function removeTag(id){
+
+    const tagList = document.querySelector('.tags-area').children
+    for (tag of tagList){
+        if (tag.id == id){
+            tag.remove()
+            break
+        }
+    } 
+   
+}
+
