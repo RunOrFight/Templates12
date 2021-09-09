2303a4560f1f96b21fdeb74ecffeaebb97d3c88b
RumOrFight <dragoinfury@gmail.com>
2021-09-09 15:24:11 +0300

    final commit

--- js/script.js
+++ js/script.js
@@ -1,40 +1,87 @@
-const checkbox = document.querySelector('.readonly-switch')
-checkbox.addEventListener('change', function(){
-    const addTagBtn = document.querySelector('.add-tag_button')
-    const input = document.querySelector('.add-tag_input')
-    const tagBtns = document.querySelectorAll('.tag-button')
-    addTagBtn.disabled = !addTagBtn.disabled
-    input.disabled = !input.disabled 
-    // tagBtns.map((elem)=>{
-    //     elem.disabled = !elem.disabled
-    // })
-    for (tagBtn of tagBtns){
-        tagBtn.disabled = !tagBtn.disabled
-    }
-})
+class TagList{
 
-function addTag(){
-    const tagList = document.querySelector('.tags-area')
-    const input = document.querySelector('.add-tag_input')
-    let currentId = '0'
-    if (tagList.lastElementChild){
-        currentId = (parseInt(tagList.lastElementChild.id) + 1)
+    constructor(){
+        this.list = document.querySelector('.tags-area')
+        this.checkbox = document.querySelector('.readonly-switch')
+        this.inputTagName = document.querySelector('.add-tag_input')
     }
-    if(input.value.trim() != ''){
-        tagList.innerHTML += `<div class="tag" id=${currentId}><p class="tag-text">${input.value}</p><button class="tag-button" onclick="removeTag(${currentId})">+</button></div>`
-    }
-    input.value = ''
-}
 
-function removeTag(id){
+    getList(){
 
-    const tagList = document.querySelector('.tags-area').children
-    for (tag of tagList){
-        if (tag.id == id){
-            tag.remove()
-            break
+        for(let tag of this.list.children){
+            console.log(`${tag.id}:${tag.textContent}`)
         }
-    } 
-   
+    }
+
+    setList(arr){
+
+        let tagList = [...arr]
+        this.list.innerHTML = ''
+        for(let tag of tagList){
+            this.addTag(tag)
+        }
+    }
+
+    addTag(str){
+        let currentId = '0'
+
+        for(let tag of str.split(/[, ]+/)){
+            if (this.list.firstElementChild){
+                currentId = (parseInt(this.list.firstElementChild.id) + 1)
+            }
+            if (tag.trim() != ''){
+                this.list.insertAdjacentHTML('afterBegin',`<div class="tag" id=${currentId}><p class="tag-text">${tag}</p><button class="tag-button" onclick="tagList.removeTag(${currentId})"><img src="img/trash.svg"></button></div>`)
+            }
+            else{
+                continue
+            }
+        }
+        this.inputTagName.value = ''
+    }
+
+    removeTag(id){
+
+        for (let tag of this.list.children){
+            if (tag.id == id || tag.textContent === id){
+                tag.remove()
+                break
+            }
+        } 
+    }
+    setReadonly(bool){
+        this.checkbox.checked = bool
+
+        document.querySelector('.add-tag_button').disabled = bool
+        document.querySelector('.add-tag_input').disabled = bool
+        for(let tag of document.querySelectorAll('.tag-button')){
+            tag.disabled = bool
+        }
+    }
+    getReadonly(){
+        return this.checkbox.checked
+    }
+
 }
 
+let tagList = new TagList()
+let newStorage = window.localStorage
+
+if(newStorage.getItem('tagList')){
+    tagList.list.innerHTML = newStorage.getItem('tagList')
+}
+
+if(newStorage.getItem('readonlyMode')){
+    tagList.setReadonly(newStorage.getItem('readonlyMode'))
+}
+else{
+    newStorage.setItem('readonlyMode', tagList.checkbox.checked)
+}
+
+const tagListObserver = new MutationObserver(()=>{
+    newStorage.setItem('tagList', tagList.list.innerHTML)
+})
+tagListObserver.observe(tagList.list, {childList:true})
+
+tagList.checkbox.addEventListener('change', ()=>{
+    newStorage.setItem('readonlyMode', tagList.checkbox.checked)
+})
